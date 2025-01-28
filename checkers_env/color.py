9ac098f4cb9cdb0d66b1ef9d6421cedf1f6b8b47
krishirajsinh-p <krishirajpuwar@gmail.com>
2025-01-28 23:44:16 +0100

    added new color and changed exisiting

--- checkers_env/color.py
+++ checkers_env/color.py
@@ -1,13 +1,35 @@
 class Color:
-    BEIGE = (245, 245, 220)
-    BROWN = (165, 42, 42)
+    # for board
+    BEIGE = (240, 217, 181)
+    BROWN = (181, 136, 99)
 
+    # for pieces
+    LIGHT_BLUE = (173, 216, 230)
+    LIGHT_RED = (250, 128, 114)
+
+    # for pieces border
+    DARK_GREEN = (0, 128, 0)
+    DARK_RED = (139, 0, 0)
+
+    # other colors
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
 
-    SILVER = (192, 192, 192)
-    GOLD = (255, 215, 0)
+    LIGHT_GRAY = (211, 211, 211)
+    LIGHT_GREEN = (144, 238, 144)
+    LIGHT_YELLOW = (255, 255, 224)
+    LIGHT_PURPLE = (221, 160, 221)
+    LIGHT_ORANGE = (255, 160, 122)
+    LIGHT_PINK = (255, 182, 193)
+    LIGHT_CYAN = (224, 255, 255)
+    LIGHT_LIME = (50, 205, 50)
+    LIGHT_TURQUOISE = (64, 224, 208)
 
-    RED = (255, 0, 0)
-    GREEN = (0, 255, 0)
-    BLUE = (0, 0, 255)
+    DARK_GRAY = (169, 169, 169)
+    DARK_YELLOW = (255, 215, 0)
+    DARK_PURPLE = (128, 0, 128)
+    DARK_ORANGE = (255, 140, 0)
+    DARK_PINK = (255, 105, 180)
+    DARK_CYAN = (0, 139, 139)
+    DARK_LIME = (0, 255, 0)
+    DARK_TURQUOISE = (0, 206, 209)
